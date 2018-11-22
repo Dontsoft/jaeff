41fa1934e76ebb5e3839e03195fd0dcd6cae6a8f
Artem Dontsov <artem@bellaza.net>
2018-11-22 23:24:10 +0100

    Added webpack, cards, basic layouts and some buttons

---
+++ production.config.js
@@ -0,0 +1,52 @@
+// webpack v4
+const path = require('path');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry:  ['./src/index.js', './src/jaeff.scss' ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jaeff.js'
+  },
+  optimization: {
+   minimizer: [
+     new UglifyJsPlugin({
+       cache: true,
+       parallel: true
+     }),
+     new OptimizeCSSAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+     })
+   ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [ 
+    new CleanWebpackPlugin('dist', {} ),
+    new MiniCssExtractPlugin({
+      filename: 'jaeff.css',
+    }),
+    new WebpackMd5Hash()
+  ]
+};
