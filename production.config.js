420d4071b0eab2dc9c178f057e9c705af8e0c3e8
Artem Dontsov <artem.dontsov@tu-ilmenau.de>
2018-11-26 13:24:43 +0100

    Moved fonts to local

--- production.config.js
+++ production.config.js
@@ -39,7 +39,17 @@ module.exports = {
       {
         test: /\.scss$/,
         use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      }
+      },
+      {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+              }
+          }]
+      },
     ]
   },
   plugins: [ 
