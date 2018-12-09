40121bd9b74ce0d34edef93a01f7bc71eba351e5
Artem Dontsov <artem@dontsoft.com>
2018-12-09 15:35:06 +0100

    Fixed replacement issue

--- plugins/gulp-sass-namespace.js
+++ plugins/gulp-sass-namespace.js
@@ -5,7 +5,7 @@ var PLUGIN_NAME = 'gulp-sass-namespace';
 module.exports = function() {
    return through.obj((file, encoding, callback) => {
       let content = file.contents.toString('utf8');
-      content = content.replace(/\.[a-zA-Z-_]+(?=\s|\{|)/g, ".jf--$&");
+      content = content.replace(/\.[a-zA-Z]{2}[a-zA-Z0-9\-\_]+(?=\s|\{|\.|:)(?!w3|org)/g, ".jf--$&");
       content = content.replace(/\.jf--\./g, ".jf--");
       file.contents = new Buffer(content, 'utf8');
       callback(null, file);
